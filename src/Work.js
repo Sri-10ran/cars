3b05e818c44037cf5c407a147bd9f014fd58d1ce
Sri-10ran <130053396+Sri-10ran@users.noreply.github.com>
2024-11-28 11:58:02 +0530

    Update Work.js

--- src/Work.js
+++ src/Work.js
@@ -11,15 +11,15 @@ function Work() {
             <Row className="gx-0 flexbox-row" style={{width:'100%',marginRight:'auto',marginLeft:'auto'}}>
                 <Col className="work-col">
                     <h2 className="work-header" style={{fontSize:'18px',textAlign:'center',fontFamily:'Montserrat'}}>Have doubts? Call</h2>
-                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.6px'}}> +91 12345 67890</h1>
+                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.6px'}}><i class="fa-solid fa-phone"></i> +91 12345 67890</h1>
                 </Col>
                 <Col className="work-col">
                     <h2 className="work-header" style={{fontSize:'18px',textAlign:'center',fontFamily:'Montserrat'}}>We are open from</h2>
-                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.4px'}}> Mon - Sat (7 a.m - 7 p.m)</h1>
+                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.4px'}}><i class="fa-regular fa-clock"></i> Mon - Sat (7 a.m - 7 p.m)</h1>
                 </Col>
                 <Col className="work-col">
                     <h2 className="work-header" style={{fontSize:'18px',textAlign:'center',fontFamily:'Montserrat'}}>Further? Drop mail to</h2>
-                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.4px'}}> laundryes@gmail.com</h1>
+                    <h1 className="work-detail" style={{fontSize:'22px',textAlign:'center',marginTop:'7px',fontFamily:'Josefin Sans',fontWeight:'640',letterSpacing:'0.4px'}}><i class="fa-solid fa-envelope-circle-check"></i> carcares@gmail.com</h1>
                 </Col>
             </Row>
         </Container>
